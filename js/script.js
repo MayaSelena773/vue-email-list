f86fc15281b3e750ac55624912afe96d9e9c46ad
MayaSelena773 <maya.bono721@gmail.com>
2022-07-04 15:28:13 +0200

    randommail

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+    var app = new Vue(
+        {
+            el: '#root',
+            data: {
+                mailsArray: []
+            },
+            methods: {
+            },
+            mounted() {
+                for(let i = 1; i <= 10; i++) {
+
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.mailsArray.push(response.data.response);
+                });
+
+                }
+            }
+        }
+    );
